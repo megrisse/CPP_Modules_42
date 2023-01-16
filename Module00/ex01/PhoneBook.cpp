876a9e4adcf4dc05fcd01d143c790f5217c6ee65
Merouane Grissen <megrisse@e1r1p2.1337.ma>
2023-01-16 02:51:37 +0100

    ex01 done

--- Module00/ex01/PhoneBook.cpp
+++ Module00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: megrisse <megrisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 14:21:08 by megrisse          #+#    #+#             */
-/*   Updated: 2023/01/12 22:05:14 by megrisse         ###   ########.fr       */
+/*   Updated: 2023/01/16 02:46:42 by megrisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,25 @@
 int main(){
     
     PhoneBook   contact;
-    int i = 0;
-    while (i < 8)
+    
+    contact.contactNumbers = 0;
+    while (1)
     {
-        contact.Contacts[i].index = i;
+        std::cout << "TYPE AN COMMAND : ADD | SEARCH | EXIT" << std::endl;
+        contact.Contacts[contact.contactNumbers].index = contact.contactNumbers;
+        std::cout << contact.contactNumbers << std::endl;
         std::string input;
-        std::cin >> input;
-        if (input == "add")
-            {contact.add(contact.Contacts, i); i++; continue;}
-        else if (input == "search")
+        if (!(std::getline(std::cin, input)))
+            return (1);
+        if (input == "ADD" && contact.contactNumbers < 8)
+            {contact.add(contact.Contacts, contact.contactNumbers); contact.contactNumbers++; continue;}
+        else if (input == "ADD" && contact.contactNumbers == 8)
+            {contact.FixPhoneBook(contact.Contacts, contact.contactNumbers); contact.add(contact.Contacts, contact.contactNumbers - 1); continue;}
+        if (input == "SEARCH")
             {contact.search(); continue;}
-        else if (input == "exit")
+        if (input == "EXIT")
             {contact.exit(); break;}
-        // continue ;
+        else
+            {std::cout << "Invalid command" << std::endl; continue;}
     }
 }
--- Module00/ex01/PhoneBook.hpp
+++ Module00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: megrisse <megrisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 14:21:11 by megrisse          #+#    #+#             */
-/*   Updated: 2023/01/13 21:54:23 by megrisse         ###   ########.fr       */
+/*   Updated: 2023/01/16 02:48:34 by megrisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,52 +30,97 @@ class	PhoneBook
 {
 public:
 	Contact	Contacts[8];
-	void	search(){
-		int	i = 0;
-		
-		std::cout << "	index	|	firstname	|	lastname	|	nickname	| number " << std::endl;
-		while (i < 8){
-			std::cout << "	" <<Contacts[i].index;
-			std::cout << "	|	" << Contacts[i].firstname << "	";
-			std::cout << "	|	" << Contacts[i].lastname << "	";
-			std::cout << "	|	" << Contacts[i].nickname << "	";
-			std::cout << "	|	" << Contacts[i].number << std::endl;
-			i++;
-		}
-	};
+	int			contactNumbers;
+	void	search();
 	void	exit(){
 		std::cout << "exit" << std::endl;
 	};
 			PhoneBook(){
-		std::cout << "PhoneBook created" << "\v" << "|";
-		for (int i = 0; i < 8; i++)
-		{	Contacts[i].firstname = '.';
-			Contacts[i].number = '.';
-			Contacts[i].index = i;
-			Contacts[i].lastname = '.';
-			Contacts[i].nickname = '.';
-			Contacts[i].secret = '.';}
-}
-			~PhoneBook(){
-		// std::cout << Contacts[0].firstname << std::endl;
-		// std::cout << Contacts[0].lastname << std::endl;
-		// std::cout << Contacts[0].nickname << std::endl;
-		// std::cout << Contacts[0].number << std::endl;
-		// std::cout << Contacts[0].index << std::endl;
-}
+		std::cout << "PhoneBook created" << std::endl;
+		//std::cout << "TYPE AN COMMAND" << std::endl;
+};
 	void	add(Contact* contact, int i);
+	void	FixPhoneBook(Contact* contact, int i);
 };
 
 void	PhoneBook::add(Contact* contact, int i){
 	
 	std::cout << "firstname : ";
-	std::cin >> contact[i].firstname;
+	if (!(std::getline(std::cin, contact[i].firstname)))
+		return ;
+	if (contact[i].firstname.length() > 10)
+		contact[i].firstname = contact[i].firstname.substr(0, 9).append(".");
+	else
+		{contact[i].firstname = contact[i].firstname.append("         "); contact[i].firstname = contact[i].firstname.substr(0, 10);}
 	std::cout << "lastname : ";
-	std::cin >> contact[i].lastname;
+	if (!(std::getline(std::cin, contact[i].lastname)))
+		return ;
+	if (contact[i].lastname.length() > 10)
+		contact[i].lastname = contact[i].lastname.substr(0, 9).append(".");
+	else
+		{contact[i].lastname = contact[i].lastname.append("         "); contact[i].lastname = contact[i].lastname.substr(0, 10);}
 	std::cout << "nickname : ";
-	std::cin >> contact[i].nickname;
+	if (!std::getline(std::cin, contact[i].nickname))
+		return ;
+	if (contact[i].nickname.length() > 10)
+		contact[i].nickname = contact[i].nickname.substr(0, 9).append(".");
+	else
+		{contact[i].nickname = contact[i].nickname.append("         "); contact[i].nickname = contact[i].nickname.substr(0, 10);}
 	std::cout << "number : ";
-	std::cin >> contact[i].number;
+	if (!std::getline(std::cin, contact[i].number))
+		return ;
+	if (contact[i].number.length() > 10)
+		contact[i].number = contact[i].number.substr(0, 9).append(".");
+	else
+		{contact[i].number = contact[i].number.append("         ");	contact[i].number = contact[i].number.substr(0, 10);}
+	std::cout << "Darkest secret : ";
+	if (!std::getline(std::cin, contact[i].secret))
+		return ;
+	if (contact[i].secret.length() > 10)
+		contact[i].secret = contact[i].secret.substr(0, 9).append(".");
+	else
+		{contact[i].secret = contact[i].secret.append("         "); contact[i].secret = contact[i].secret.substr(0, 10);}
 }
+
+void	PhoneBook::FixPhoneBook(Contact* contact, int contactNumbers){
+	int i = 0;
+	while (i < contactNumbers - 1){
+		contact[i] = contact[i + 1];
+		Contacts[i].index = i;
+		i++;
+	}
+};
+
+void	PhoneBook::search(){
+	int	i = 0;
+	std::string str;
+
+	if (contactNumbers == 0)
+		std::cout << "no contact saved yet : type ADD to create one" << std::endl;
+	else
+	{
+	std::cout << "   index  | firstname | lastname  | nickname " << std::endl;	
+	while (i < contactNumbers){
+		std::cout << "_____________________________________________" << std::endl;
+		std::cout << "     " << Contacts[i].index << "    ";
+		std::cout << "| " << Contacts[i].firstname;
+		std::cout << "| " << Contacts[i].lastname;
+		std::cout << "| " << Contacts[i].nickname << std::endl;
+		i++;
+	}
+	std::cout << "type an contact index" << std::endl;
+	std::getline(std::cin, str);
+	i = std::stoi(str);
+	if (i >= contactNumbers)
+		std::cout << "no contact with this index" << std::endl;
+	else
+	{
+	std::cout << "firstname : " << Contacts[i].firstname << "| ";
+	std::cout << "lastname : " << Contacts[i].lastname << "| ";
+	std::cout << "nickname : " << Contacts[i].nickname << "| ";
+	std::cout << "number : " << Contacts[i].number << "| ";
+	std::cout << "secret : " << Contacts[i].secret << std::endl;}}
+};
+
 
 #endif
