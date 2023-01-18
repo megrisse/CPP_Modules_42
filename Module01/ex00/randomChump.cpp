e5ce67f365fabbd63010664572e95ce2534d3ad4
Merouane Grissen <mgrissen9@gmail.com>
2023-01-19 00:12:12 +0100

    randomChump destructed

--- Module01/ex00/randomChump.cpp
+++ Module01/ex00/randomChump.cpp
@@ -6,12 +6,14 @@
 /*   By: megrisse <megrisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 20:18:09 by megrisse          #+#    #+#             */
-/*   Updated: 2023/01/18 22:33:55 by megrisse         ###   ########.fr       */
+/*   Updated: 2023/01/19 00:09:52 by megrisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 void    randomChump(std::string name){
-    newZombie(name)->announce();
+    Zombie *Zomb = newZombie(name);
+    Zomb->announce();
+    delete Zomb;
 }
